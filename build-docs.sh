7b351cb1d32ceefd43316eb3be39d2f824a8519b
David Barragán Merino <david.barragan@kaleidos.net>
2016-05-22 19:35:36 +0200

    Improve build script

--- build-docs.sh
+++ build-docs.sh
@@ -8,7 +8,10 @@ make pdf || exit 1
 rm -rf /tmp/taiga-doc-dist || exit 1
 cp -r dist /tmp/taiga-doc-dist || exit 1
 git checkout gh-pages || exit 1
+git pull || exit 1
+git reset --hard || exit 1
 rm -rf dist || exit 1
 mv /tmp/taiga-doc-dist dist || exit 1
 git add --all dist || exit 1
 git commit -a -m "Update doc" || exit 1
+git push || exit 1
