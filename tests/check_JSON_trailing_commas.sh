b74944e7e765691ba474afaebd2d449cd8676bd2
Yamila Moreno <yamila.ms@gmail.com>
2021-05-06 12:20:19 +0200

    (chore) fix shebang

--- tests/check_JSON_trailing_commas.sh
+++ tests/check_JSON_trailing_commas.sh
@@ -1,10 +1,10 @@
+#!/bin/sh
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2021-present Kaleidos Ventures SL
-
-#!/bin/sh
 
 # This script is used in two ways:
 # 1. To check a single .adoc file as soon as it is modified (via. Guardfile)
