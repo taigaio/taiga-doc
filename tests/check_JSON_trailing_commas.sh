1867338bb5d34b76a169d872f0ec7bd82bec1406
David Barragán Merino <david.barragan@kaleidos.net>
2024-03-27 13:37:01 +0100

    fix: company name: Kaleidos Ventures SL now is Kaleidos INC

--- tests/check_JSON_trailing_commas.sh
+++ tests/check_JSON_trailing_commas.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021-present Kaleidos Ventures SL
+# Copyright (c) 2021-present Kaleidos INC
 
 # This script is used in two ways:
 # 1. To check a single .adoc file as soon as it is modified (via. Guardfile)
